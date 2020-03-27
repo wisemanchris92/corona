315eb88c09d2cdd961c745d427443e00bfa825de
ChrisWiseman <wisemanchris92@gmail.com>
2020-03-27 10:35:23 -0400

    working on project folder/file structure. added .gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,9 @@
+'''
+This file initializes your 
+application and brings together 
+all of the various components.
+'''
+
 from flask import Flask
 
 
---
+++ app/models.py
@@ -0,0 +1,6 @@
+'''
+This is where you define the 
+models of your application. 
+This may be split into several 
+modules in the same way as views.py.
+'''
